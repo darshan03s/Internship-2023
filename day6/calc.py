8dbd7ea71e1ae9795655c3a3a6910af112688cd3
xdxds <darshans2727@gmail.com>
2023-11-04 16:09:32 +0530

    6

---
+++ day6/calc.py
@@ -0,0 +1,26 @@
+def sum():
+    print('Sum')
+    a = int(input("Enter first number : "))
+    b = int(input("Enter second number : "))
+    print(a+b)
+def diff():
+    print('Difference')
+    a = int(input("Enter first number : "))
+    b = int(input("Enter second number : "))
+    print(a-b)
+def prod():
+    print('Product')
+    a = int(input("Enter first number : "))
+    b = int(input("Enter second number : "))
+    print(a*b)
+def quo():
+    print('Quotient')
+    a = int(input("Enter first number : "))
+    b = int(input("Enter second number : "))
+    print(a/b)
+def mod():
+    print('Modulus')
+    a = int(input("Enter first number : "))
+    b = int(input("Enter second number : "))
+    print(a%b)
+    
